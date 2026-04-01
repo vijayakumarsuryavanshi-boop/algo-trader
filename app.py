ae6357af13a3b6b1c1a2fa27b4f1ef2684005e83
Vijayakumar Suryavanshi <vijayakumar.suryavanshi@gmail.com>
2026-04-02 04:25:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1607,7 +1607,7 @@ class CoinDCXBridge:
         try:
             ts = int(round(time.time() * 1000))
             if symbol == "XAUUSD":
-                base_coin = "XAU" # Changed from GOLD
+                base_coin = "XAUT" # Changed from GOLD
             else:
                 base_coin = symbol.replace("USDT", "").replace("USD", "").replace("INR", "")
             
@@ -1759,7 +1759,7 @@ class CoinDCXBridge:
         try:
             pair = symbol.replace("USD", "USDT") if symbol.endswith("USD") and not symbol.endswith("USDT") else symbol
             if symbol == "XAUUSD":
-                pair = "XAUUSDT"
+                pair = "XAUTUSD"
             res_map = {"1m": "1", "5m": "5", "15m": "15", "30m": "30", "1h": "60", "4h": "240", "1d": "1D"}
             resolution = res_map.get(interval, "5")
             end = int(time.time())
